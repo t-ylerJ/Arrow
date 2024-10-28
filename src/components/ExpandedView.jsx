7a2400db832b7c96456492199d7bec20f34e76d0
Tyler Johnson <tylerj0937@gmail.com>
2024-10-28 17:43:50 -0500

    Remove comments and console logs

--- src/components/ExpandedView.jsx
+++ src/components/ExpandedView.jsx
@@ -1,19 +1,14 @@
 import React, { useState, useContext, useRef } from 'react';
-
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import { IoClose } from 'react-icons/io5';
-
 import ImageGalleryButton from './ImageGalleryButton';
 import ImageThumbnails from './ImageThumbnails';
-
 import AppContext from '../context/AppContext';
-
 import missing from '../images/missing.svg?url';
 
 function ExpandedView({ switchImage }) {
   const [zoomed, setZoomed] = useState(false);
   const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
-
   const imgRef = useRef(null);
 
   const {
@@ -34,17 +29,13 @@ function ExpandedView({ switchImage }) {
     big.width = imgRef.current.naturalWidth * 2.5;
     big.height = imgRef.current.naturalHeight * 2.5;
   }
-  // console.log(big);
-
   const { photos } = styles[selectedStyle];
-  // photos = [...styles[selectedStyle].photos, ...styles[selectedStyle].photos, ...styles[selectedStyle].photos];
 
   return (
     <div
       className={`relative w-full h-full overflow-hidden flex flex-col gap-2 items-center select-none ${zoomed ? 'cursor-[url(../images/minus.svg),auto] justify-center max-w-none max-h-none' : 'cursor-[url(../images/plus.svg),auto]'}`}
       onClick={() => setZoomed(!zoomed)}
       onPointerMove={(e) => {
-        // console.log(e.clientX, e.clientY);
         setMousePos({ x: e.clientX, y: e.clientY });
       }}
     >
@@ -63,7 +54,6 @@ function ExpandedView({ switchImage }) {
       {!zoomed ? (
         <>
           <ImageThumbnails horizontal />
-          {/* <ImageThumbnails orientation="horizontal" /> */}
           <ImageGalleryButton testid="expanded-left" ariaLabel="prev-image" flip styles="left-8" cb={() => switchImage(-1)}><FaArrowLeft /></ImageGalleryButton>
           <ImageGalleryButton testid="expanded-right" ariaLabel="next-image" styles="right-8" cb={() => switchImage(1)}><FaArrowRight /></ImageGalleryButton>
           <button className="absolute right-6 top-6 bg-white text-black opacity-80 rounded-full cursor-pointer" aria-label="close" onClick={hideModal}><IoClose size={32} /></button>
